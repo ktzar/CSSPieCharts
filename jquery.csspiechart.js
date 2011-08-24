2c85f54faf56c11262f2f802431cb81032fff42b
kTzAR <miguelmail@gmail.com>
2011-08-24 15:47:47 +0100

    Documentation

--- jquery.csspiechart.js
+++ jquery.csspiechart.js
@@ -1,32 +1,46 @@
+/*!
+ * CSSPieChart jQuery plugin
+ * Copyright 2011, Miguel L Gonzalez
+ * Licensed under the GPL Version 3 license.
+ * http://mentadreams.com
+ *
+ * csspiechart jQuery function, takes object options as argument with properties:
+ * values: absolute values to be represented in the pieChart
+ * size: size of the pie chart (width and height)
+ * colors: array of valid CSS colors, i.e. #ff0, rgb(255,40,40). For now it's not recommended to use non-opaque colours
+ * TODO: modify the CSS for width and height
+ * TODO: accept an option for explicit percentage values
+ * 
+ */
 (function($){
     $.fn.csspiechart = function(options) {
 
+        //Default values
         var defaults = {
             values: [10,20,30],
-            size: 300,
-            colors: ['red','orange','yellow','green','#0ff', 'blue','purple'],
+            size: 200,
+            colors: ['red','orange','yellow','green','#0ff', 'blue','purple'], //the rainbow colors as default
         };
 
+        //Merge the default options witht the values in the params
         var options = $.extend(defaults, options);
 
+        //Do for all the elements
         return this.each(function() {
             obj = $(this);
             var body = obj.html();
-            console.log(options.values);
             sum = 0;
             total = 0;
+            //Get the sum of values
             for(val in options.values) {
                 total+=options.values[val];
             }
             angles = new Array();
             obj.html('');
             for(val in options.values) {
-                r = parseInt(Math.random()*255);
-                g = parseInt(Math.random()*255);
-                b = parseInt(Math.random()*255);
                 angle = parseInt(36000*(options.values[val]/total))/100;
                 angles.push(angle);
-                //put 2
+                //If the angle of this slice is > 180 degrees we need 2 divs for displaying that
                 if (angle > 180){
                     obj.append('<div style="-webkit-transform:rotate('+sum+'deg)" class="front"><div style="background:'+options.colors[val]+';-webkit-transform:rotate('+(180)+'deg)" class="pie"></div></div>');
                     obj.append('<div style="-webkit-transform:rotate('+(sum+180)+'deg)" class="front"><div style="background:'+options.colors[val]+';-webkit-transform:rotate('+(angle-180)+'deg)" class="pie"></div></div>');
@@ -35,7 +49,7 @@
                 }
                 sum+=angle;
             }
-            console.log(angles);
+            //The back of the Pie, for the border and shadow
             obj.append('<div class="holder back"></div>');
         });
     };
